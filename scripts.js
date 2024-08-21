19b29c60522ee3f3e9b8c2d8fc10728de63873d2
gurmanpreetsinghaulakh <147732899+gurmanpreetsinghaulakh@users.noreply.github.com>
2024-08-21 11:13:06 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contactForm');
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+
+        const name = document.getElementById('name').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const message = document.getElementById('message').value.trim();
+
+        if (name === "" || email === "" || message === "") {
+            alert("Please fill out all fields.");
+            return;
+        }
+
+        alert("Message sent successfully!");
+        contactForm.reset();
+    });
+});
